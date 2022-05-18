9be401bc893c81a43a2b2cdb09ba5928cf8a8d7c
Jason Rueckert <jrueckert@costco.com>
2022-05-18 14:45:33 -0700

    refactored file structures

--- code/level.py
+++ code/level.py
@@ -19,16 +19,19 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == "x":
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == "p":
-                    self.player = Player(
-                        (x, y), [self.visible_sprites], self.obstacle_sprites
-                    )
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     for col_index, col in enumerate(row):
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
+        #         if col == "x":
+        #             Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+        #         if col == "p":
+        #             self.player = Player(
+        #                 (x, y), [self.visible_sprites], self.obstacle_sprites
+        #             )
+        self.player = Player(
+            (2000, 1430), [self.visible_sprites], self.obstacle_sprites
+        )
 
     def run(self):
         # update and draw game
@@ -47,7 +50,7 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2()
 
         # creating the floor
-        self.floor_surf = pygame.image.load("./graphics/tilemap/ground.png").convert()
+        self.floor_surf = pygame.image.load("../graphics/tilemap/ground.png").convert()
         self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
 
     def custom_draw(self, player):
